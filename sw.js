da644ae56c24e20dee84c920445356c6b41dc3a0
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-10 16:36:05 +0000

    docs(hangar): specify quantum overclock duration formulas for arcade powerups

--- sw.js
+++ sw.js
@@ -105,3 +105,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 3/134]: docs(hangar): define local storage persistence schema for banked coin balances (2023-12-10T08:44:26.165Z)
 // [Expansion Doc Milestone 4/134]: docs(hangar): add expanded magazine tier cost table and capacity progression math (2023-12-10T11:21:39.248Z)
 // [Expansion Doc Milestone 5/134]: docs(hangar): document ion thruster vertical velocity acceleration multipliers (2023-12-10T13:58:52.330Z)
+// [Expansion Doc Milestone 6/134]: docs(hangar): specify quantum overclock duration formulas for arcade powerups (2023-12-10T16:36:05.413Z)
