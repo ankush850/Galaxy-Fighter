85a8165b99ec952b8fdc157f5298fd38790fbf70
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-11 17:05:00 +0530

    feat(trophies): add in-game animated achievement unlocked toast notification

--- sw.js
+++ sw.js
@@ -86,3 +86,4 @@ self.addEventListener('fetch', (event) => {
 // [Build Revision 4/20]: feat(hangar): add dynamic level pills and real-time upgrade cost calculations (2023-12-06T16:30:00)
 // [Build Revision 5/20]: feat(trophies): create trophy room modal and live achievement tracker (2023-12-08T09:15:00)
 // [Build Revision 6/20]: feat(trophies): define 8 milestone achievements with reward coin bounties (2023-12-09T13:40:00)
+// [Build Revision 7/20]: feat(trophies): add in-game animated achievement unlocked toast notification (2023-12-11T17:05:00)
