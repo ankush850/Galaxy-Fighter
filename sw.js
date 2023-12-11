2c7fdfead23e2501715236073e5a1022a88bb789
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-11 16:11:03 +0000

    docs(aircraft): document cosmo cruiser battleship reinforced hull and bonus magazine slot

--- sw.js
+++ sw.js
@@ -114,3 +114,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 12/134]: docs(aircraft): document blue stealth aerodynamic speed bonuses and reduced hitbox profile (2023-12-11T08:19:23.909Z)
 // [Expansion Doc Milestone 13/134]: docs(aircraft): document thunder gold heavy plasma blast radius and muzzle flash VFX (2023-12-11T10:56:36.992Z)
 // [Expansion Doc Milestone 14/134]: docs(aircraft): document viper recon fighter magnetic coin attractor coils (2023-12-11T13:33:50.075Z)
+// [Expansion Doc Milestone 15/134]: docs(aircraft): document cosmo cruiser battleship reinforced hull and bonus magazine slot (2023-12-11T16:11:03.157Z)
