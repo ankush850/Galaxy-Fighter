bb238377225f0dcb72f09a9800861f0c9f82f436
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-12 00:02:42 +0000

    docs(overdrive): modularize fury overdrive ultimate ability into features/fury_overdrive.md

--- sw.js
+++ sw.js
@@ -117,3 +117,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 15/134]: docs(aircraft): document cosmo cruiser battleship reinforced hull and bonus magazine slot (2023-12-11T16:11:03.157Z)
 // [Expansion Doc Milestone 16/134]: docs(aircraft): add class hierarchy diagram for player aircraft entity inheritance (2023-12-11T18:48:16.240Z)
 // [Expansion Doc Milestone 17/134]: docs(aircraft): document title screen fighter card selection state synchronization (2023-12-11T21:25:29.323Z)
+// [Expansion Doc Milestone 18/134]: docs(overdrive): modularize fury overdrive ultimate ability into features/fury_overdrive.md (2023-12-12T00:02:42.406Z)
