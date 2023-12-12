dfa030129b387a356ad9a0e975e0dfc6b28cffae
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-12 23:37:40 +0000

    docs(survival): document 15-kill wave completion threshold and scaling formulas

--- sw.js
+++ sw.js
@@ -126,3 +126,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 24/134]: docs(overdrive): document mobile touch overdrive button pulsing animation triggers (2023-12-12T15:46:00.902Z)
 // [Expansion Doc Milestone 25/134]: docs(overdrive): add state machine transition diagram for fury charging and discharge (2023-12-12T18:23:13.984Z)
 // [Expansion Doc Milestone 26/134]: docs(survival): modularize endless survival wave mode into features/endless_survival.md (2023-12-12T21:00:27.067Z)
+// [Expansion Doc Milestone 27/134]: docs(survival): document 15-kill wave completion threshold and scaling formulas (2023-12-12T23:37:40.150Z)
