fa45638dfaff14d274aea6d7f5857ef29d888903
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-13 12:43:45 +0000

    docs(survival): document enemy spawn delay decay curves as survival waves advance

--- sw.js
+++ sw.js
@@ -131,3 +131,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 29/134]: docs(survival): document periodic alien dreadnought boss raids on every 3rd wave (2023-12-13T04:52:06.315Z)
 // [Expansion Doc Milestone 30/134]: docs(survival): define survival wave bonus score calculations and coin multipliers (2023-12-13T07:29:19.398Z)
 // [Expansion Doc Milestone 31/134]: docs(survival): specify survival mode UI wave counters and banner notifications (2023-12-13T10:06:32.481Z)
+// [Expansion Doc Milestone 32/134]: docs(survival): document enemy spawn delay decay curves as survival waves advance (2023-12-13T12:43:45.563Z)
