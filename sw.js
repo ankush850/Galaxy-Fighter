99a30eb9c61977f057107958671df8440a4b955b
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-18 21:07:26 +0000

    docs(audio): document smart EMP nuke white noise and low-frequency sweep synthesis

--- sw.js
+++ sw.js
@@ -180,3 +180,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 78/134]: docs(audio): document hyper rocket exponential sawtooth synthesis curve (80Hz to 600Hz) (2023-12-18T13:15:47.368Z)
 // [Expansion Doc Milestone 79/134]: docs(audio): document cosmic hoverboard square-wave power chord synthesis parameters (2023-12-18T15:53:00.451Z)
 // [Expansion Doc Milestone 80/134]: docs(audio): document super magnet modulated sine-wave attractor chime (2023-12-18T18:30:13.533Z)
+// [Expansion Doc Milestone 81/134]: docs(audio): document smart EMP nuke white noise and low-frequency sweep synthesis (2023-12-18T21:07:26.616Z)
