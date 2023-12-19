6fd956ec552748ba7e9d2c4b20978a0b184b9e4a
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-19 12:50:45 +0000

    docs(arch): document storage contracts and deserialization safety in hangar subsystem

--- sw.js
+++ sw.js
@@ -186,3 +186,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 84/134]: docs(android): document AndroidManifest hardware acceleration and immersive sticky flags (2023-12-19T04:59:05.864Z)
 // [Expansion Doc Milestone 85/134]: docs(android): document PWA WebAPK offline service worker caching strategy (2023-12-19T07:36:18.947Z)
 // [Expansion Doc Milestone 86/134]: docs(arch): create dedicated hangar persistence architecture in architecture/HANGAR_AND_PERSISTENCE_ARCHITECTURE.md (2023-12-19T10:13:32.030Z)
+// [Expansion Doc Milestone 87/134]: docs(arch): document storage contracts and deserialization safety in hangar subsystem (2023-12-19T12:50:45.112Z)
