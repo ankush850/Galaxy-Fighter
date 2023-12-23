58a3d77539d6adde7de72c68d1e4fa70ee9936dc
ankush850 <ankushsinghrawat154@gmail.com>
2023-12-23 03:18:56 +0000

    docs(aircraft): document viper recon fighter magnetic coin attractor coils (iteration 2)

--- sw.js
+++ sw.js
@@ -219,3 +219,4 @@ self.addEventListener('fetch', (event) => {
 // [Expansion Doc Milestone 117/134]: docs(aircraft): document red falcon tactical dogfighting traits and rapid muzzle recovery (iteration 2) (2023-12-22T19:27:17.593Z)
 // [Expansion Doc Milestone 118/134]: docs(aircraft): document blue stealth aerodynamic speed bonuses and reduced hitbox profile (iteration 2) (2023-12-22T22:04:30.676Z)
 // [Expansion Doc Milestone 119/134]: docs(aircraft): document thunder gold heavy plasma blast radius and muzzle flash VFX (iteration 2) (2023-12-23T00:41:43.759Z)
+// [Expansion Doc Milestone 120/134]: docs(aircraft): document viper recon fighter magnetic coin attractor coils (iteration 2) (2023-12-23T03:18:56.842Z)
